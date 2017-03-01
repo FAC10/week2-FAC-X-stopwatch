080ca1e67e64c4247edd864a7a50f3383438e81f
Jessica Salmon <jessicasalmon05@gmail.com>
2017-03-01 15:15:03 +0000

    built function to find current time in miliseconds

--- Test/test.js
+++ Test/test.js
@@ -1,6 +1,16 @@
 // initial test used to check if QUnit was working
-test('add(1,1) should equal 2', function(assert) {
+QUnit.test('add(1,1) should equal 2', function(assert) {
   var result = add(1, 1); //no change/coins just an empty array
   var expected = 2;
   assert.deepEqual(result, expected);
 }); // use deepEqual for arrays see: https://api.qunitjs.com/deepEqual/
+
+QUnit.test('function should return current time in milliseconds', function(assert) {
+  var date = new Date();
+  var currentTime = date.getTime();
+  assert.equal(getCurrentTime(), currentTime);
+})
+
+// QUnit.test('If 2, returns 2', function(asserts) {
+//   asserts.equal(fizzBuzz(2), 2, "Success! 2 does equal 2")
+// })
--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 function add(x, y) {
   return x + y;
 }
+
+function getCurrentTime() {
+  var today = new Date();
+  return today.getTime();
+}
+
+getCurrentTime();
+console.log(getCurrentTime());
