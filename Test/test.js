1484009faf2c2d3c335064dfe671ae5778d825a5
Jessica Salmon <jessicasalmon05@gmail.com>
2017-03-01 16:09:05 +0000

    comment out functions/ test to start new implementation strategy

--- Test/test.js
+++ Test/test.js
@@ -5,30 +5,17 @@ QUnit.test('add(1,1) should equal 2', function(assert) {
   assert.deepEqual(result, expected);
 }); // use deepEqual for arrays see: https://api.qunitjs.com/deepEqual/
 
-QUnit.test('function should return current time in milliseconds', function(assert) {
-  var date = new Date();
-  var currentTime = date.getTime();
-  assert.equal(getCurrentTime(), currentTime);
-});
 
+// Time practice
+// QUnit.test('function should return current time in milliseconds', function(assert) {
+//   var date = new Date();
+//   var currentTime = date.getTime();
+//   assert.equal(getCurrentTime(), currentTime);
+// });
+//
 // QUnit.test('function should convert time in milliseconds to current date', function(assert) {
 //   var date = new Date();
-//   var currentDate = date.getTime().toString();
+//   var currentTime = date.getTime();
+//   var currentDate = new Date(currentTime).toString();
 //   assert.equal(convertMilliseconds(), currentDate);
 // });
-
-QUnit.test('function should convert time in milliseconds to current date', function(assert) {
-  var date = new Date();
-  var currentTime = date.getTime();
-  var currentDate = new Date(currentTime).toString();
-  assert.equal(convertMilliseconds(), currentDate);
-});
-
-
-
-// QUnit.test('If 2, returns 2', function(asserts) {
-//   asserts.equal(fizzBuzz(2), 2, "Success! 2 does equal 2")
-// })
-
-
-// currentDate.toString('yyyy-MM-dd');
--- scripts.js
+++ scripts.js
@@ -2,18 +2,17 @@ function add(x, y) {
   return x + y;
 }
 
-function getCurrentTime() {
-  var today = new Date();
-  return today.getTime();
-}
-
-getCurrentTime();
+// function getCurrentTime() {
+//   var today = new Date();
+//   return today.getTime();
+// }
+// getCurrentTime();
 // console.log(getCurrentTime());
-
-function convertMilliseconds(){
-  var currentTime = getCurrentTime();
-  return new Date(currentTime).toString();
-}
-
-convertMilliseconds();
-console.log(convertMilliseconds());
+//
+//
+// function convertMilliseconds(){
+//   var currentTime = getCurrentTime();
+//   return new Date(currentTime).toString();
+// }
+// convertMilliseconds();
+// console.log(convertMilliseconds());
