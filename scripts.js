6ae3ef399d9c0cc6e5c46fa03aedcc8d1ff1387e
Jessica Salmon <jessicasalmon05@gmail.com>
2017-03-02 15:37:32 +0000

    add function that counts in milliseconds

--- scripts.js
+++ scripts.js
@@ -7,12 +7,26 @@ document.getElementById('start').addEventListener('click', changeButton, false);
 
 function changeButton() {
   document.querySelector('.startButton').innerHTML = 'Started';
-  console.log('Hi');
+  // console.log('Hi');
 }
 
 document.getElementById('stop').addEventListener('click', changeButtonAgain, false);
 
 function changeButtonAgain() {
   document.querySelector('.stopButton').innerHTML = 'Stopped';
-  console.log('Hi again');
+  // console.log('Hi again');
+}
+
+// function for counting milliseconds:
+document.getElementById('start').addEventListener('click', startTimer, false);
+
+function startTimer() {
+  var startTime = Date.now();
+
+  var interval = setInterval(function() {
+    var elapsedTime = Date.now() - startTime;
+    document.getElementById('millisecond').innerHTML = (elapsedTime / 1000).toString().substr(-3, 2);
+  }, 10);
+
+  // console.log(document.getElementById('millisecond'))
 }
