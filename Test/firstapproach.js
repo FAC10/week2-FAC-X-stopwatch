aec33c3701e6bcc1edfd677841f9afd215fdc2f7
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-03-02 11:45:07 +0000

    add our first approach attempted for testing for implementation based on the date object

---
+++ Test/firstapproach.js
@@ -0,0 +1,14 @@
+// first approach
+// Time practice
+// QUnit.test('function should return current time in milliseconds', function(assert) {
+//   var date = new Date();
+//   var currentTime = date.getTime();
+//   assert.equal(getCurrentTime(), currentTime);
+// });
+//
+// QUnit.test('function should convert time in milliseconds to current date', function(assert) {
+//   var date = new Date();
+//   var currentTime = date.getTime();
+//   var currentDate = new Date(currentTime).toString();
+//   assert.equal(convertMilliseconds(), currentDate);
+// });
