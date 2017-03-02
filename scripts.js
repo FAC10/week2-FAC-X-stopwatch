aba5f67d269541108d6524c913d25bfcee89d5ed
Jessica Salmon <jessicasalmon05@gmail.com>
2017-03-02 16:15:34 +0000

    make function that sets the state to false and clears the interval when stop button is clicked

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+var active = false;
+var interval;
+
 function add(x, y) {
   return x + y;
 }
@@ -21,12 +24,23 @@ function changeButtonAgain() {
 document.getElementById('start').addEventListener('click', startTimer, false);
 
 function startTimer() {
+  active = true;
+
   var startTime = Date.now();
 
-  var interval = setInterval(function() {
+  interval = setInterval(function() {
     var elapsedTime = Date.now() - startTime;
     document.getElementById('millisecond').innerHTML = (elapsedTime / 1000).toString().substr(-3, 2);
   }, 10);
 
   // console.log(document.getElementById('millisecond'))
+}
+
+
+// function for stopping timer:
+document.getElementById('stop').addEventListener('click', stopTimer, false);
+
+function stopTimer() {
+  active = false;
+  clearInterval(interval);
 }
