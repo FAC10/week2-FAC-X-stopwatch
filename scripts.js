e046417a24ee50ea91ca94bcd426810a756d8e2f
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-03-02 11:53:13 +0000

    add eventListener to start button to change content onClick

--- scripts.js
+++ scripts.js
@@ -2,37 +2,6 @@ function add(x, y) {
   return x + y;
 }
 
-// function getCurrentTime() {
-//   var today = new Date();
-//   return today.getTime();
-// }
-// getCurrentTime();
-// console.log(getCurrentTime());
-//
-//
-// function convertMilliseconds(){
-//   var currentTime = getCurrentTime();
-//   return new Date(currentTime).toString();
-// }
-// convertMilliseconds();
-// console.log(convertMilliseconds());
-
-
-
-//function for counting milliseconds:
-
-// var startTime = Date.now();
-//
-// var interval = setInterval(function() {
-//   var elapsedTime = Date.now() - startTime;
-//   document.getElementById('millisecond').innerHTML = (elapsedTime / 100).toFixed(0);
-// }, 100);
-//
-// var startButtonOnClick = document.getElementById('start').addEventListener('click', function() {
-//   document.querySelector('.startButton').innerHTML = 'Started';
-//   console.log('Hi');
-// });
-//
 document.getElementById('start').addEventListener('click', changeButton, false);
 
 function changeButton() {
